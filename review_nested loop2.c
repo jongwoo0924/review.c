7e9f10d52e340115949253267f1a0340f85bfea6
jongwoo0924 <117901685+jongwoo0924@users.noreply.github.com>
2022-11-27 13:49:55 +0900

    Update review_nested loop2.c

--- review_nested loop2.c
+++ review_nested loop2.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void)
+{
+  int i,j,n;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    for(j=i;j<=n;j++)
+    {
+      printf("%d ",j);
+    }
+    printf("\n");
+  }
+  return 0;
+}/*출력 : n==5
+1 2 3 4 5
+2 3 4 5
+3 4 5
+4 5
+5*/
+
+
+int main(void)
+{
+  int i,j,n;
+  scanf("%d",&n);
+  for(i=n;i>=1;i--)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("%d ",j);
+    }
+    printf("\n");
+  }
+  return 0;
+}/*출력 : n==5
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+*/
